b794f512ca003563555779338229c0f6feba1ed4
lyszt <kalliddel@gmail.com>
2025-05-05 11:29:37 -0300

    Commands are translated since ShadowBot is made for a brazilian public

--- Bot/Modules/Actions/QuoteFetcher.py
+++ Bot/Modules/Actions/QuoteFetcher.py
@@ -1,4 +1,5 @@
 import requests
+import translate
 
 class QuoteFetcher:
     def __init__(self, console):
@@ -15,7 +16,7 @@ class QuoteFetcher:
         if response.status_code == requests.codes.ok:
             self.console.log(f"Acquired random quote: STATUS CODE {response.status_code}")
             quote = response.json()
-            self.quote = quote['data']['content']
+            self.quote = translate.Translator(from_lang='en', to_lang='pt-br').translate(quote['data']['content'])
             return self
         else:
             self.console.log("Failed at getting random quote.")
