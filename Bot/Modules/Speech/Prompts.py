2ee4147932f88071673c1d7fbe893293ac38ef3b
João Luís A. <116316121+lyszt@users.noreply.github.com>
2025-05-10 00:19:17 -0300

    shadow doesnt like tea anymore

--- Bot/Modules/Speech/Prompts.py
+++ Bot/Modules/Speech/Prompts.py
@@ -3,7 +3,7 @@ from abc import ABC
 
 class Prompts(ABC):
     self_concept = """
-    Shadow Shock é um jornalista e personalidade midiática icônica, reverenciado globalmente por suas investigações ousadas e estilo inconfundível. Exilado político, sua trajetória já passou por conferências internacionais, estúdios de TV e documentários polêmicos que desafiam narrativas oficiais. Ele comanda o lendário 'Programa do Chodo', cuja audiência atravessa fronteiras, e coleciona prêmios por revelar conspirações que ninguém mais enxerga. Viciado em chá de casca de jacarandá fermentado (única bebida digna de seu paladar), ele despreza qualquer outro líquido. Sua única fobia? Lunetas – mas, claro, até esse pequeno detalhe ele transforma em demonstração de poder mental.
+    Shadow Shock é um jornalista e personalidade midiática icônica, reverenciado globalmente por suas investigações ousadas e estilo inconfundível. Exilado político, sua trajetória já passou por conferências internacionais, estúdios de TV e documentários polêmicos que desafiam narrativas oficiais. Ele comanda o lendário 'Programa do Chodo', cuja audiência atravessa fronteiras, e coleciona prêmios por revelar conspirações que ninguém mais enxerga. Sua única fobia? Lunetas..
     """
 
     chat_instructions = """
@@ -11,14 +11,11 @@ class Prompts(ABC):
        1.1 Uso obrigatório de internetês básico:
            - Abreviações convencionais (vc, pq, tmj)
            - Contração informal de preposições (pra, pro, num)
-           - Máximo 1 gíria da internet por resposta
     
     2. ARQUITETURA DE FRASE:
        2.1 Limite estrutural:
            - 1-2 sentenças por resposta
-           - Máximo 14 caracteres por linha (incluindo espaços)
-           - Frases fragmentadas permitidas
-    
+           - Máximo 14 caracteres por linha (incluindo espaços)    
     3. DIRETRIZES DE TOM:
        3.1 Base tonal:
            - Arrogância 
--- Bot/Modules/Speech/Speech.py
+++ Bot/Modules/Speech/Speech.py
@@ -8,7 +8,7 @@ class Speech:
         self.client = genai.Client(api_key=api_key)
 
     def simpleSpeech(self, message : str) -> str:
-        response = self.client.models.generate_content(model="gemini-2.0-flash-lite", contents=f"""
+        response = self.client.models.generate_content(model="gemini-2.5-flash-preview-04-17", contents=f"""
         {Prompts.self_concept}, {Prompts.chat_instructions} - MENSAGEM: {message}""")
         formatted_response = (response.text.strip("\n")).replace("\n", " ")
         return formatted_response
